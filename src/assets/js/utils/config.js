cfcfefc572a67027aac1bda16a0a4d64c1363a0d
Luuxis <61792917+luuxis@users.noreply.github.com>
2025-10-03 15:06:13 +0200

    Update config and instance URLs in config.js
    Changed config and news endpoints to use simplified URLs. Updated instance list retrieval to fetch from '/instances' instead of '/files' and removed assignment of 'name' property to instance objects.

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -8,8 +8,8 @@ const nodeFetch = require("node-fetch");
 const convert = require('xml-js');
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
-let config = `${url}/launcher/config-launcher/config.json`;
-let news = `${url}/launcher/news-launcher/news.json`;
+let config = `${url}/config`;
+let news = `${url}/news`;
 
 class Config {
     GetConfig() {
@@ -24,14 +24,13 @@ class Config {
     }
 
     async getInstanceList() {
-        let urlInstance = `${url}/files`
+        let urlInstance = `${url}/instances`
         let instances = await nodeFetch(urlInstance).then(res => res.json()).catch(err => err)
         let instancesList = []
         instances = Object.entries(instances)
 
         for (let [name, data] of instances) {
             let instance = data
-            instance.name = name
             instancesList.push(instance)
         }
         return instancesList
