d1f1a751e0cc124eae87554fadbdc7274d27b855
Luuxis <61792917+luuxis@users.noreply.github.com>
2025-11-30 00:17:08 +0100

    Update icon processing and rename launcher in docs
    Refactored iconSet method in build.js for improved buffer handling and updated Jimp usage. Changed launcher name from 'CraftLaunch' to 'Selvania Launcher' in README.md.

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const builder = require('electron-builder')
 const JavaScriptObfuscator = require('javascript-obfuscator');
 const nodeFetch = require('node-fetch')
 const png2icons = require('png2icons');
-const Jimp = require('jimp');
+const { Jimp, JimpMime } = require('jimp');
 
 const { preductname } = require('./package.json');
 
@@ -69,7 +69,7 @@ class Index {
                 artifactName: "${productName}-${os}-${arch}.${ext}",
                 extraMetadata: { main: 'app/app.js' },
                 files: ["app/**/*", "package.json", "LICENSE.md"],
-                directories: { 
+                directories: {
                     "output": "dist"
                 },
                 compression: 'normal',
@@ -109,7 +109,7 @@ class Index {
                         arch: "universal"
                     },
                     {
-                        target: "zip", 
+                        target: "zip",
                         arch: "universal"
                     }]
                 },
@@ -151,15 +151,15 @@ class Index {
     }
 
     async iconSet(url) {
-        let Buffer = await nodeFetch(url)
-        if (Buffer.status == 200) {
-            Buffer = await Buffer.buffer()
-            const image = await Jimp.read(Buffer);
-            Buffer = await image.resize(256, 256).getBufferAsync(Jimp.MIME_PNG)
-            fs.writeFileSync("src/assets/images/icon.icns", png2icons.createICNS(Buffer, png2icons.BILINEAR, 0));
-            fs.writeFileSync("src/assets/images/icon.ico", png2icons.createICO(Buffer, png2icons.HERMITE, 0, false));
-            fs.writeFileSync("src/assets/images/icon.png", Buffer);
-            console.log('new icon set')
+        const response = await nodeFetch(url)
+        if (response.status == 200) {
+            const buffer = await response.buffer()
+            let image = await Jimp.read(buffer);
+            image = await image.resize({ w: 256, h: 256 }).getBuffer(JimpMime.png);
+            fs.writeFileSync("src/assets/images/icon.icns", png2icons.createICNS(image, png2icons.BILINEAR, 0));
+            fs.writeFileSync("src/assets/images/icon.ico", png2icons.createICO(image, png2icons.HERMITE, 0, false));
+            fs.writeFileSync("src/assets/images/icon.png", image);
+            // console.log('new icon set')
         } else {
             console.log('connection error')
         }
